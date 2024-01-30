422ca1c7e3fca93a9a168464b9a0446d7aa4559f
Kiran TUMMALA <69224313+kirannissi@users.noreply.github.com>
2024-01-30 21:02:28 +1100

    error fix

--- src/test/steps/auth.setup.ts
+++ src/test/steps/auth.setup.ts
@@ -3,22 +3,17 @@ import {Given ,When} from "@cucumber/cucumber";
 import {chromium, Page, Browser, test} from "@playwright/test";
 let browser: Browser;
 let page: Page;
-let baseurl= process.env.BASE_URL as String;
-let username= process.env.USER_NAME as String;
-let password= process.env.PASSWORD as String;
 
-
-When('I Log into the UT Environment', async ({page}) => {
+Given('User Log into the UT Environment', async ({page}) => {
    
     browser= await chromium.launch({headless:false});
     page= await browser.newPage();
-    await page.goto(baseurl);
+    await page.goto(process.env.BASE_URL);
     await page.locator('.MuiButton-root').click();
     await page.locator('input[type=submit]').click();
-    await page.locator('input[type=email]').fill(username);
+    await page.locator('input[type=email]').fill(process.env.USER_NAME);
     await page.locator('input[type=submit]').click();
-    await page.locator('input[name=passwd]').fill(password);
+    await page.locator('input[name=passwd]').fill(process.env.PASSWORD);
     await page.locator('input[type=submit]').click();
     await page.locator('input[type=submit]').click();
-   
 });
